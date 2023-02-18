82213bd78ff97a14471135e71520a0d33d31ca4b
Mvuzo <fanelesbonge.mzulu@gmail.com>
2023-02-18 14:04:55 -0800

    C files 5 and 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * descripton: main - Prints out a string to stdout
+ * Descripton: main - Prints out a string to stdout.
  *
- * Return: 0 (Success)
+ * Return: 0 if success.
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
+
 }
