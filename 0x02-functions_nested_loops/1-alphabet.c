91c69f4da647ef37f9526ce0c8d4aa4e66dc681a
Mvuzo <fanelesbonge.mzulu@gmail.com>
2023-02-23 13:08:00 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Aphabets in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alph);
+		alpha++;
+	}
+
+	_putchar ('\n');
+}
